bcb319bd466eab6a6af15454dade98d4b5aeb40b
nishida <k.nishida@vistage-llc.com>
2020-08-18 13:46:59 +0900

    Homeコンポーネント再作成

---
+++ src/components/HomeWork.vue
@@ -0,0 +1,19 @@
+<template>
+    <section
+        class="work"
+        id="work"
+        style="background:#000;color:#fff;height:300px"
+    >
+        <h1>This is Work</h1>
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.work {
+    text-align: center;
+}
+</style>
