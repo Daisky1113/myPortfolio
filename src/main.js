18da08941c4450efa7095a7820a9e2d39495686e
nishida <k.nishida@vistage-llc.com>
2020-08-20 14:03:19 +0900

    0820レイアウト完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import vueSmoothScroll from "vue-smooth-scroll";
 
-Vue.config.productionTip = false
+Vue.use(vueSmoothScroll);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
